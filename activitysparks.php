3c98403c8fe3f550deaf51eae6681cb692774e2b
pantsonhead <pantsonhead@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-19 21:27:06 +0000

    fixed version number
    git-svn-id: https://plugins.svn.wordpress.org/activitysparks/trunk@147315 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- activitysparks.php
+++ activitysparks.php
@@ -4,7 +4,7 @@
 Plugin name: Activity Sparks
 Plugin URI: http://www.pantsonhead.com/wordpress/activitysparks/
 Description: A widget to display a customizable sparkline graph of post and/or comment activity.
-Version: 0.2.1
+Version: 0.3
 Author: Greg Jackson
 Author URI: http://www.pantsonhead.com
 
